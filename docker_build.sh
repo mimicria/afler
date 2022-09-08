0ab52a999af5fd30981585f8f5acc7059b9a5c96
Vladimir Ryabokon <vova@cbi57.ru>
2022-09-08 06:15:42 +0000

    new tags

--- docker_build.sh
+++ docker_build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker build --no-cache=True -t mimicria/afler:ub2204 -f Dockerfile .
-#docker build -t mimicria/afler:latest -f Dockerfile .
+#docker build -t mimicria/afler:ub2204 -f Dockerfile .
 docker push mimicria/afler:ub2204
